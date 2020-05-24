9e8222523730e377291b6271b17efb28e6c9e39d
Cristian Ceban <cristian.ceban@etu.u-pec.fr>
2020-05-24 20:01:42 +0300

    added bclose,updated bopen.

--- bio.c
+++ bio.c
@@ -17,18 +17,65 @@
 
 BFD* 
 bopen(const char *path, const char *mode){
-	BFD *bp = malloc(sizeof(bp));
-	//int fd;
-	if(strcmp("w",mode)==0){
-		printf("w");
-	}
-	else if(strcmp("r",mode)==0){
-		printf("r");
-	}
-	else if(strcmp("a",mode)==0){
-		printf("a");
-	}
+BFD *bp ;
+	//Check if memory was alocated.
+	if((bp=malloc(sizeof(BFD)))==NULL)
+		return NULL;
 	
+	//Check mode and opens file descriptor with the coresponding flags.
+
+	//write
+	if(strcmp("w",mode)==0){
+		//Open file
+		bp->fd = open(path,O_WRONLY | O_TRUNC | O_CREAT,0644);
+		//Store the mode
+		bp->mode =  BWRITE;
+		//Store the size of the file
+		bp->fsize = lseek(bp->fd,0,SEEK_END);
+		//Reset the cursor in the file
+		lseek(bp->fd,0,SEEK_SET);
+		//Store the begining of the cursor
+		bp->cur = SEEK_SET;	
+	}
+	//read
+	else if(strcmp("r",mode)==0){
+		//open file
+		bp->fd = open(path,O_RDONLY);
+		//store the mode
+		bp->mode = BREAD;
+		//Store the size of the file
+		bp->fsize = lseek(bp->fd,0,SEEK_END);
+		//Reset the cursor in the file
+		lseek(bp->fd,0,SEEK_SET);
+		//Store the begining of the cursor
+		bp->cur = SEEK_SET;
+	}
+	//append
+	else if(strcmp("a",mode)==0){
+		//Open file
+		bp->fd = open(path,O_WRONLY| O_APPEND | O_CREAT,0644);
+		//store the mode
+		bp->mode = BWRITE;
+		//Store the size of the file
+		bp->fsize = lseek(bp->fd,0,SEEK_END);
+		//store the current cursor possition.
+		//Because it is in append mode it will be after the final of the file.
+		bp->cur = bp->fsize;
+	}
+	//if it's neither from the modes.
+	else {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	//error with file descriptor
+	if(bp->fd < 0)
+		return NULL;
+	
+	//intialise the cursors in the buffer;
+	bp->pos = 0;
+	bp->end = 0;
+		
 	return bp;
 }
 
@@ -55,5 +102,21 @@ beof(BFD *bfd){
 
 int 
 bclose(BFD *bfd){
-	return 0;
+	//Check if file was opened
+	if(bfd->mode!=BREAD && bfd->mode!=BWRITE){
+		errno = EBADF;
+		return -1;
+	}
+
+	//If it's in the writen mode
+	if(bfd->mode==BWRITE)
+		bflush(bfd);
+	//close the file descriptor
+	if((close(bfd->fd))==0){
+		free(bfd);
+		//Free alocated memory for bfd
+		return 0;
+	}
+	errno = BERROR;
+	return -1;
 }
